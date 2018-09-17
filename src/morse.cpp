432eeccc70f12c1748a495bd559d6d9a29272467
fzy <zfuaj@connect@ust.hk>
2018-09-17 15:11:30 +0800

    final

--- src/morse.cpp
+++ src/morse.cpp
@@ -4,19 +4,34 @@ using namespace chibios_rt;
 
 
 void print_code(const morse_code_t& code){
-  if(!flag_change){
-  for(int i=0;i<code.length;i++){
-
+  if(flag_change){
     palSetPad(GPIOA,GPIOA_LED);
-    chThdSleepMilliseconds(200);
-    if(flag_change) break;
-
+  for(int i=0;i<code.length;i++){
     palClearPad(GPIOA,GPIOA_LED);
     if(code.frame[i]==DASH)  chThdSleepMilliseconds(600);
     else if(code.frame[i]==DOT) chThdSleepMilliseconds(200);
-    if(flag_change) break;
+
+    if(!flag_change){
+      palSetPad(GPIOA,GPIOA_LED);
+      break;
+      }
+
+    palSetPad(GPIOA,GPIOA_LED);
+    chThdSleepMilliseconds(200);
+
+    if(!flag_change){
+      palSetPad(GPIOA,GPIOA_LED);
+      break;
+      }
+
+    }
+  if(flag_change){
+    code_state++;
+    code_state=code_state%FRAME_LENGTH;
+    }
   }
-}
-  if(!flag_change)  code_state++;
-  palSetPad(GPIOA,GPIOA_LED);
+  else chThdSleepMilliseconds(200);
+
+
+  //palSetPad(GPIOA,GPIOA_LED);
 }
